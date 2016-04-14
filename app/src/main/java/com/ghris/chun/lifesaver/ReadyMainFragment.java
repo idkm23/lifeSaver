149b5de1ffb4d365c9905c1283806554832bdf90
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-04-14 10:59:29 -0400

    Added Dialog and Switching State by hitting the Icon (-- This doesn't work for now).
    The Navbar is still there under navigation-list-item layout...Cannot
    popup smoothly yet. But you still can try to slide it into the screen

--- app/src/main/java/com/ghris/chun/lifesaver/ReadyMainFragment.java
+++ app/src/main/java/com/ghris/chun/lifesaver/ReadyMainFragment.java
@@ -1,21 +1,29 @@
 package com.ghris.chun.lifesaver;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
+import java.util.zip.Inflater;
 
 /**
  * Created by chris on 4/1/2016.
@@ -28,14 +36,47 @@ public class ReadyMainFragment extends Fragment implements View.OnTouchListener 
     private boolean ticking = false;
     private int seconds , minutes;
 
+    // Add Image View Variable
+    private ImageView navButton, infoButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.ready_main_fragment, container, false);
+        final View rootView = inflater.inflate(R.layout.ready_main_fragment, container, false);
         counter_textview = (TextView) rootView.findViewById(R.id.countdown_clock);
         ready = (Button) rootView.findViewById(R.id.ready_button);
         rootView.findViewById(R.id.ready_button).setOnTouchListener(this);
+
+        navButton = (ImageView) rootView.findViewById(R.id.nav_button);
+        infoButton = (ImageView) rootView.findViewById(R.id.info_button);
+
+        // set on click for those view
+        navButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Change to Nav -- Menu State state
+                // Adapter object must stay inside onclick
+                // Cannot create global Adapter or Static Adapter
+
+//                Fragment menu = new MenuFragment();
+//                FragmentManager fm = getFragmentManager();
+//                android.support.v4.app.FragmentTransaction transaction = fm.beginTransaction();
+//                transaction.replace(R.id.ready_main, menu);
+//                transaction.commit();
+
+                // Todo: Change State -- My way messed up - Hung
+            }
+        });
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change to info state
+                MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(getFragmentManager());
+                adapter.getItem(MainFragmentPagerAdapter.INFO_POS);
+            }
+        });
 
         return rootView;
     }
@@ -81,7 +122,25 @@ public class ReadyMainFragment extends Fragment implements View.OnTouchListener 
                 }
 
                 public void onFinish() {
-                    counter_textview.setText("-:--");
+                    // counter_textview.setText("-:--");
+                    new AlertDialog.Builder(getContext())
+                            .setTitle("On Hold!")
+                            .setMessage("Are you good? Do you want to hold another 3 mins?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // continue with delete
+                                    timer.onTick(180000);
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // do nothing
+                                    //// TODO: 4/14/2016 Add state for the "Sending SMS"
+
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
                 }
             };
         }
